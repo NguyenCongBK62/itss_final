43b2c3b12405e67dcb53749ca922b73aec450c5d
CongNV-Training <nguyen.van.cong@sun-asterisk.com>
2021-09-09 08:35:15 +0700

    itss final exam

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import useStorage from "./hooks/useStorage";
 
 function App() {
+  const [inputName, studentIndex, handleChange, onClick] = useStorage();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="listName listStudent">
+        <p>学生一覧: [Huyen, Hoa, Hung, Long]</p>
+      </div>
+      <div className="listStudent">
+        <div >検索名前 :</div>
+        <input onChange={handleChange} />
+      </div>
+      <button onClick={onClick} className="myButton">確定</button>
+      <div className="listStudent">検索名前: {inputName}</div>
+
+      <div className="listName listStudent">
+        位置: {studentIndex}
+      </div>
     </div>
   );
 }
